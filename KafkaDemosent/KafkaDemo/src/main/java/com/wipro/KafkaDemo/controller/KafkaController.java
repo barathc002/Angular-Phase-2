063f67e531b55f96c577c5cbe4165f1479e439dc
barathc002 <128670504+barathc002@users.noreply.github.com>
2025-03-10 12:07:11 +0530

    Add files via upload

---
+++ KafkaDemosent/KafkaDemo/src/main/java/com/wipro/KafkaDemo/controller/KafkaController.java
@@ -0,0 +1,22 @@
+package com.wipro.KafkaDemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wipro.KafkaDemo.service.KafkaProducerService;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService producerService;
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        producerService.sendMessage(message);
+        return "Message sent: " + message;
+    }
+}
+
